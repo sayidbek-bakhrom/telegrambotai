b6dc3ad14c225902a5de878ddec2d181f944bd6a
Sayidbek <87595572+sayidbek-bakhrom@users.noreply.github.com>
2025-07-28 07:44:26 +0000

    Refactor: Moved config.py to app/, removed unused bot/check.py

--- app/config.py
+++ app/config.py
@@ -6,8 +6,3 @@ load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 if not HF_TOKEN:
     raise ValueError("HF_TOKEN is not set!")
-
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-if not TELEGRAM_BOT_TOKEN:
-    raise ValueError("TELEGRAM_BOT_TOKEN is not set!")
